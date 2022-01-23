587ea619ee8c665bd6dab01efa72d38c5fa7e0e1
Wang Guosheng <wguosh@gmail.com>
2022-01-24 06:45:32 +0800

    backup empty wiki

--- mdconvert.py
+++ mdconvert.py
@@ -1,5 +1,24 @@
+class MdConverter():
+
+    def __init__(self, fname):
+        self.fname = fname
+        self.ftype = "omd"
+        self.text = self.reader(self, fname)
+
+    def reader(self, fname):
+        with open(fname, 'r') as f:
+            text = f.read()
+
+        if ftype == "xmd":
+            self.xmdreader(fname)
+        elif ftype == "omd":
+            self.omdreader(fname)
+
+
 # Wikilinks
 r'\[[^\]]*\]\(\#[^\)]+\)'
+
+# URL
 
 # RefCite
 r'<<ref[^>]*>>'
